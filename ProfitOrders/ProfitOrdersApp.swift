04161d7d397d31c219938d126bc7ba589bad5fa6
Siddu <npnbhanusiddartha@gmail.com>
2024-02-22 21:00:18 +0530

    Initial Commit

---
+++ ProfitOrders/ProfitOrdersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfitOrdersApp.swift
+//  ProfitOrders
+//
+//  Created by Siddu on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ProfitOrdersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
